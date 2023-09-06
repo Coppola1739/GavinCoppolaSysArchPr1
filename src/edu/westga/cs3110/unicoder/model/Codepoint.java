4fb5679fba37635d0d6d7080c987a8ae48fe41b7
gavcop <gavin.coppola@outlook.com>
2023-09-06 10:46:13 -0400

    Initial commit

---
+++ src/edu/westga/cs3110/unicoder/model/Codepoint.java
@@ -0,0 +1,10 @@
+package edu.westga.cs3110.unicoder.model;
+
+public class Codepoint {
+    private String codepoint;
+
+    public Codepoint(String hexadecimalString){
+        this.codepoint = hexadecimalString;
+    }
+
+}
