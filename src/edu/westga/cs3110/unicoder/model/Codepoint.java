224abfeb22bb100c070dcd67f5a471985641554a
gavcop <gavin.coppola@outlook.com>
2023-09-06 11:17:09 -0400

    Adds codepoint constructor with tests

--- src/edu/westga/cs3110/unicoder/model/Codepoint.java
+++ src/edu/westga/cs3110/unicoder/model/Codepoint.java
@@ -1,10 +1,25 @@
 package edu.westga.cs3110.unicoder.model;
 
 public class Codepoint {
+
+    public static String VALID_HEXADECIMAL_STRING = "^[A-Fa-f0-9]+$";
     private String codepoint;
 
-    public Codepoint(String hexadecimalString){
+    public Codepoint(String hexadecimalString) {
+        if (hexadecimalString == null) {
+            throw new NullPointerException("Hexadecimal value cannot be null");
+        }
+        if (hexadecimalString.isBlank()) {
+            throw new IllegalArgumentException("Hexadecimal value cannot be blank");
+        }
+        if (!hexadecimalString.matches(VALID_HEXADECIMAL_STRING)) {
+            throw new IllegalArgumentException("Hexadecimal value must be valid format");
+        }
         this.codepoint = hexadecimalString;
+    }
+
+    public String getCodepoint() {
+        return this.codepoint;
     }
 
 }
---
+++ src/edu/westga/cs3110/unicoder/tests/model/codepoint/TestCodepointConstructor.java
@@ -0,0 +1,37 @@
+package edu.westga.cs3110.unicoder.tests.model.codepoint;
+
+import edu.westga.cs3110.unicoder.model.Codepoint;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestCodepointConstructor {
+
+    @Test
+    public void testValidCodepointConstructor() {
+        Codepoint codepoint = new Codepoint("1F682");
+        assertEquals(codepoint.getCodepoint(), "1F682");
+    }
+
+    @Test
+    public void testNullCodepointConstructor() {
+        assertThrows(NullPointerException.class, () -> {
+            new Codepoint(null);
+        });
+    }
+
+    @Test
+    public void testBlankCodepointConstructor() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Codepoint("");
+        });
+    }
+
+    @Test
+    public void testInvalidHexaCodepointConstructor(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Codepoint("Q12!");
+        });
+    }
+
+}
